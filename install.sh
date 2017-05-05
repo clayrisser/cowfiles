1073f1bb8a51fa70036db321ab2d9c5e634610aa
Jam Risser <jamrizzi@users.noreply.github.com>
2017-05-05 10:36:55 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ if [ "$UID" -ne 0 ]; then
   exit 1
 fi
 
-curl -LO https://github.com/jamrizzi/cowfiles/blob/master/cows/frog.cow /usr/share/cowsay/cows
+curl -LO https://raw.githubusercontent.com/jamrizzi/cowfiles/master/cows/frog.cow /usr/share/cowsay/cows
 
 echo "Installation finished"
